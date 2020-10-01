1f24aa04e85ac1d459d03812081458187f6e6594
iusamasajjad <i.usamasajjad@gmail.com>
2020-10-01 05:58:21 +0500

    code comments added

--- index.cpp
+++ index.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <conio.h>
 int main() {
+	// Printing asterisks 'U' with nested loops and conditional statements.
 	for(int row=0; row<7; row++) {
 		for(int col=0; col<9; col++) {
 			if(col==0 || col==7 || row==6) {
@@ -12,6 +13,7 @@ int main() {
 		printf("\n");
 	}
 	printf("\n");
+	// Printing asterisks 'S' with nested loops and conditional statements.
 	for(int row=0; row<=6; ++row) {
 		for(int col=0; col<9; ++col)
 			if(row==0 || (row<=2 && col==0) || row==3 || (row>=4 && col==8) || row==6) {
@@ -22,6 +24,7 @@ int main() {
 		printf("\n");
 	}
 	printf("\n");
+	// Printing asterisks 'A' with nested loops and conditional statements.
 	for(int row=3; row<9; ++row) {
 		for(int col=0; col<9; ++col) {
 			if((row+col==8 && row>=4) || (row==col && row>=4) || (row==6 && col>=3 && col<=5)) {
@@ -33,6 +36,7 @@ int main() {
 		printf("\n");
 	}
 	printf("\n");
+	// Printing asterisks 'M' with nested loops and conditional statements.
 	for (int row=0; row<7; ++row) {
 		for (int col=0; col<9; ++col) {
 			if(col==0 || (row==col && row<=4 ) || (row+col==8 && row<=4) || col==8) {
@@ -44,6 +48,7 @@ int main() {
 		}
 		printf("\n");
 	}
+	// Printing asterisks 'A' with nested loops and conditional statements.
 	for(int row=3; row<9; ++row) {
 		for(int col=0; col<9; ++col) {
 			if((row+col==8 && row>=4)|| (row==col && row>=4)|| (row==6 && col>=3 && col<=5 )) {
